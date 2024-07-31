0b7ff350eef7fa068a0b2b671dd31f18a63bfa89
Grinik767 <grigorikiseleff@yandex.ru>
2024-08-01 00:18:33 +0500

    save tree

--- drive_manager.py
+++ drive_manager.py
@@ -1,10 +1,11 @@
 import os
 import lzma
+from utils import *
 
 
 class DriveManager:
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, workspace_path):
+        self.path = workspace_path
         self.repo_path = os.path.join(self.path, '.kit')
 
     def save_file(self, file_hash):
@@ -21,8 +22,22 @@ class DriveManager:
         with open(os.path.join(self.repo_path, 'Objects', folder, name), 'wb') as file:
             file.write(compressed_data)
 
+    def save_tree(self, tree_hash: str):
+        index_path = path.join(self.path, "INDEX")
+        hash_folder = path.join(self.repo_path, 'Objects', tree_hash[:2], tree_hash[2:])
+        os.makedirs(hash_folder)
+        with open(index_path, 'r') as f:
+            for line in f:
+                filepath, file_hash = line.split()
+                folder_name = path.split(filepath)[:-1][0]
+                folder_path = path.join(hash_folder, folder_name)
+                if folder_name:
+                    os.makedirs(folder_path, exist_ok=True)
+                with open(path.join(folder_path, path.basename(filepath)), 'w') as f:
+                    f.write(file_hash)
+
     def save_commit(self, hash):
-        pass #TODO
+        pass  # TODO
 
     def load_file(self, file_hash):
         folder = file_hash[:2]
@@ -38,7 +53,7 @@ class DriveManager:
         folder = tree_hash[:2]
         name = tree_hash[2:]
 
-        pass #TODO
+        pass  # TODO
 
     def __get_original_file_location(self, hash):
         with open(os.path.join(self.repo_path, 'INDEX'), 'r') as index:
