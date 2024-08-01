01b58ad57037590e3635a9626452294dc3e50b74
Nikolai Lesovich <lesovich2005@mail.ru>
2024-08-01 16:57:29 +0500

    version_control.py refactor

--- drive_manager.py
+++ drive_manager.py
@@ -5,14 +5,14 @@ from utils import *
 
 class DriveManager:
     def __init__(self, workspace_path):
-        self.path = workspace_path
-        self.repo_path = os.path.join(self.path, '.kit')
+        self.workspace_path = workspace_path
+        self.repo_path = os.path.join(self.workspace_path, '.kit')
 
     def save_file(self, local_path, file_hash):
         folder = file_hash[:2]
         name = file_hash[2:]
 
-        with open(os.path.join(self.path, local_path), 'rb') as file:
+        with open(os.path.join(self.workspace_path, local_path), 'rb') as file:
             data = bytes(file.read())
             compressed_data = lzma.compress(data)
 
@@ -44,9 +44,79 @@ class DriveManager:
 
         return lzma.decompress(compressed_data)
 
-    def __get_tree_hash(self, commit_hash):
-        folder = commit_hash[:2]
-        name = commit_hash[2:]
+    def save_index_files(self):
+        index_path = os.path.join(self.repo_path, 'INDEX')
+        with open(index_path, 'r') as index:
+            for line in index.readlines():
+                path, hash = line.rstrip().split(' ')
+                self.save_file(path, hash)
 
-        with open(os.path.join(self.repo_path, 'Objects', folder, name), 'r') as commit:
-            return commit.readlines()[3]
+    def write(self, local_path, data):
+        with open(os.path.join(self.workspace_path, local_path), 'w') as file:
+            file.write(data)
+
+    def write_commit_data(self, commit_id, username, datetime, description, tree, parent):
+        os.makedirs(os.path.join(self.repo_path, 'Objects', commit_id[:2]), exist_ok=True)
+        with open(os.path.join(self.repo_path, 'Objects', commit_id[:2], commit_id[2:]), 'w') as commit:
+            commit.write(f"{username}\n")
+            commit.write(f"{datetime}\n")
+            commit.write(f"{description}\n")
+            commit.write(f"{tree}\n")
+            commit.write(f"{parent}")
+
+    def write_index_data(self, local_path: str, seed: int):
+        index_path = os.path.join(self.repo_path, 'INDEX')
+        file_path = os.path.join(self.workspace_path, local_path)
+
+        with open(index_path, 'a') as index:
+            if not path.isdir(file_path):
+                index.write(f"{local_path} {Utils.get_file_hash(file_path, xxh3_128(seed=seed)).hexdigest()}\n")
+                return
+
+            for root, _, files in os.walk(file_path):
+                for file in files:
+                    file_full_path = path.join(root, file)
+                    relative_path = path.relpath(file_full_path, start=self.workspace_path)
+                    self.write_index_data(relative_path, seed)
+
+    def initialize_directories(self):
+        os.makedirs(self.repo_path, exist_ok=True)
+        os.makedirs(os.path.join(self.repo_path, 'Objects'), exist_ok=True)
+        os.makedirs(os.path.join(self.repo_path, 'Refs'), exist_ok=True)
+        os.makedirs(os.path.join(self.repo_path, 'Refs', 'heads'), exist_ok=True)
+
+        open(os.path.join(self.repo_path, 'INDEX'), 'w').close()
+
+    def get_head(self):
+        head_path = os.path.join(self.repo_path, 'HEAD')
+
+        if not os.path.exists(head_path):
+            return None
+
+        with open(head_path, 'r') as head_file:
+            return head_file.readline()
+
+    def get_seed(self):
+        seed_path = os.path.join(self.repo_path, 'SEED')
+
+        if not os.path.exists(seed_path):
+            return None
+
+        with open(seed_path, 'r') as head_file:
+            return int(head_file.read())
+
+    def get_last_commit_id(self, head):
+        if head is None:
+            return None
+
+        with open(os.path.join(self.repo_path, 'HEAD'), 'r') as head:
+            branch_path = head.readline()
+
+        with open(os.path.join(self.repo_path, branch_path), 'r') as branch_path:
+            return branch_path.readline().rstrip()
+
+    def is_exist(self, local_path):
+        return os.path.exists(os.path.join(self.workspace_path, local_path))
+
+    def remove(self, local_path):
+        os.remove(os.path.join(self.workspace_path, local_path))
--- version_control.py
+++ version_control.py
@@ -1,4 +1,4 @@
-import os
+from os import path
 from drive_manager import DriveManager
 from errors.NothingToCommitError import NothingToCommitError
 from errors.RepositoryAlreadyExistError import RepositoryAlreadyExistError
@@ -10,138 +10,65 @@ from datetime import datetime
 
 
 class VersionControl:
-    def __init__(self, workspace_path, username):
-        self.path = workspace_path
-        self.repo_path = os.path.abspath(os.path.join(workspace_path, ".kit"))
+    def __init__(self, username, workspace_path):
         self.username = username
-        self.drive = DriveManager(workspace_path)
-        self.head = self.__get_head()
-        self.seed = self.__get_seed()
-        self.current_id = self.__get_current_id()
+        self.workspace_path = workspace_path
+        self.repo_path = path.abspath(path.join(workspace_path, ".kit"))
+
+        self.drive = DriveManager(self.workspace_path)
+        self.head = self.drive.get_head()
+        self.seed = self.drive.get_seed()
+        self.current_id = self.drive.get_last_commit_id(self.head)
 
     def init(self):
-        if os.path.exists(self.repo_path):
+        if self.drive.is_exist('.kit'):
             raise RepositoryAlreadyExistError
 
-        self.head = os.path.join('Refs', 'heads', 'main')
-        self.seed = randint(10000000, 99999999)
-        self.__create_directories()
-        open(os.path.join(self.repo_path, 'INDEX'), 'w').close()
+        self.head = path.join('Refs', 'heads', 'main')
+        self.seed = randint(10**7, 10**8 - 1)
+
+        self.drive.initialize_directories()
         self.commit("initial commit")
 
-        with open(os.path.join(self.repo_path, 'HEAD'), 'w') as head:
-            head.write(self.head)
-
-        with open(os.path.join(self.repo_path, 'SEED'), 'w') as seed:
-            seed.write(f'{self.seed}')
-
-        with open(os.path.join(self.repo_path, 'Refs', 'heads', 'main'), 'w') as main:
-            main.write(f'{self.current_id}')
-
-        self.__update_branch_head()
+        self.drive.write(path.join('.kit', 'HEAD'), self.head)
+        self.drive.write(path.join('.kit', 'SEED'), str(self.seed))
+        self.drive.write(path.join('.kit', self.head), self.current_id)
 
     def add(self, local_path: str):
-        index_path = os.path.join(self.repo_path, 'INDEX')
-        file_path = os.path.join(self.path, local_path)
-
-        with open(index_path, 'a') as index:
-            if not os.path.isdir(file_path):
-                index.write(f"{local_path} {Utils.get_file_hash(file_path, xxh3_128(seed=self.seed)).hexdigest()}\n")
-                return
-
-            for root, _, files in os.walk(file_path):
-                for file in files:
-                    file_full_path = os.path.join(root, file)
-                    relative_path = os.path.relpath(file_full_path, start=self.path)
-                    self.add(relative_path)
+        self.drive.write_index_data(local_path, self.seed)
 
     def commit(self, description: str):
-        index_path = os.path.join(self.path, 'INDEX')
+        index_path = path.join('.kit', 'INDEX')
 
-        if self.current_id is not None and not os.path.exists(index_path):
+        if self.current_id is not None and not self.drive.is_exist(index_path):
             raise NothingToCommitError
 
         commit_id = xxh3_128(self.username + description + datetime.now().isoformat(), seed=self.seed).hexdigest()
-        os.makedirs(os.path.join(self.repo_path, 'Objects', commit_id[:2]), exist_ok=True)
         tree_hash = Utils.get_tree_hash_with_index_update(self.repo_path, self.seed)
 
-        with open(os.path.join(self.repo_path, 'Objects', commit_id[:2], commit_id[2:]), 'w') as commit:
-            commit.write(f"{self.username}\n")
-            commit.write(f"{datetime.now()}\n")
-            commit.write(f"{description}\n")
-            commit.write(f"{tree_hash.hexdigest()}\n")
-            commit.write(f"{self.current_id}")
+        self.drive.write_commit_data(commit_id, self.username, datetime.now(), description, tree_hash.hexdigest(), self.current_id)
 
-            self.current_id = commit_id
-            self.__update_branch_head()
+        self.current_id = commit_id
+        self.drive.write(path.join('.kit', self.head), self.current_id)
 
-        with open(index_path, 'r') as index:
-            for line in index.readlines():
-                path, hash = line.rstrip().split(' ')
-                self.drive.save_file(path, hash)
+        self.drive.save_index_files()
 
         if self.current_id is not None:
             self.drive.save_tree(tree_hash.hexdigest())
 
-        if os.path.exists(index_path):
-            os.remove(index_path)
+        if self.drive.is_exist(index_path):
+            self.drive.remove(index_path)
 
     def branch(self, name):
-        branch_path = os.path.join(self.repo_path, 'Refs', 'heads', name)
+        branch_path = path.join('.kit', 'Refs', 'heads', name)
 
-        if os.path.exists(branch_path):
+        if self.drive.is_exist(branch_path):
             raise BranchAlreadyExitsError
 
-        with open(branch_path, 'w') as branch:
-            branch.write(self.current_id)
+        self.drive.write(branch_path, self.current_id)
 
     def checkout(self):
         pass #TODO
 
     def log(self):
         pass #TODO
-
-    def __update_branch_head(self):
-        with open(os.path.join(self.repo_path, self.head), 'w') as branch:
-            branch.write(f'{self.current_id}')
-
-    def __create_directories(self):
-        os.makedirs(self.repo_path, exist_ok=True)
-        os.makedirs(os.path.join(self.repo_path, 'Objects'), exist_ok=True)
-        os.makedirs(os.path.join(self.repo_path, 'Refs'), exist_ok=True)
-        os.makedirs(os.path.join(self.repo_path, 'Refs', 'heads'), exist_ok=True)
-
-    def __get_current_tree_hash(self):
-        folder = self.current_id[:2]
-        name = self.current_id[2:]
-
-        with open(os.path.join(self.repo_path, 'Objects', folder, name), 'r') as commit:
-            return commit.readlines()[3].rstrip()
-
-    def __get_head(self):
-        head_path = os.path.join(self.repo_path, 'HEAD')
-
-        if not os.path.exists(head_path):
-            return None
-
-        with open(head_path, 'r') as head_file:
-            return head_file.readline()
-
-    def __get_seed(self):
-        seed_path = os.path.join(self.repo_path, 'SEED')
-
-        if not os.path.exists(seed_path):
-            return None
-
-        with open(seed_path, 'r') as head_file:
-            return int(head_file.read())
-
-    def __get_current_id(self):
-        if self.head is None:
-            return None
-
-        with open(os.path.join(self.repo_path, 'HEAD'), 'r') as head:
-            branch_path = head.readline()
-
-        with open(os.path.join(self.repo_path, branch_path), 'r') as branch_path:
-            return branch_path.readline().rstrip()
